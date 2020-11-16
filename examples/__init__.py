4409284d9709ef24930a199d54ff6a504abc4a61
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-11-16 14:52:26 +0000

    Update math.md

--- examples/__init__.py
+++ examples/__init__.py
@@ -3,4 +3,6 @@ from pathlib import Path
 
 from torchdevice import set_cuda_visible_devices
 
-num_devices =
+num_devices = int(os.environ.get('NUM_DEVICES', 0))
+if num_devices > 0:
+    set_cuda_visible_devic
