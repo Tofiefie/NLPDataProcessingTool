4d9521592b73da41ad4d9815c72dab85d01ccb99
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-11-20 07:26:44 +0000

    add ssh

--- examples/__init__.py
+++ examples/__init__.py
@@ -5,4 +5,8 @@ from torchdevice import set_cuda_visible_devices
 
 num_devices = int(os.environ.get('NUM_DEVICES', 0))
 if num_devices > 0:
-    set_cuda_visible_devic
+    set_cuda_visible_devices(n=num_devices)
+
+app_dir = Path(__file__).resolve().parent
+project_dir = app_dir.parent
+app_name = app_
