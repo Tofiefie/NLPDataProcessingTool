2c07b551c2e301239b0caffb98ecffedd1e0d0d6
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-12-03 18:40:31 +0000

    mmc: sdhci-s3c: fix the wrong number of max bus clocks

--- scripts/gen-mbart.sh
+++ scripts/gen-mbart.sh
@@ -6,4 +6,6 @@ targets=()
 
 for BATCH in 1024; do
   for SMOOTH in 0 0.1; do
-    for LR in 3e-3
+    for LR in 3e-3 7e-3 3e-4 7e-4 3e-5 7e-5; do
+      for BETA2 in 0.98 0.999; do
+        for DECAY in 1e-1 1e-2 1
