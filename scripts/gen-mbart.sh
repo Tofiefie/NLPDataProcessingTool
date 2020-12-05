15756ab39f5f1943afe12d3f63381e9d0590873a
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-12-05 06:19:07 +0000

    Update snake.html

--- scripts/gen-mbart.sh
+++ scripts/gen-mbart.sh
@@ -8,4 +8,9 @@ for BATCH in 1024; do
   for SMOOTH in 0 0.1; do
     for LR in 3e-3 7e-3 3e-4 7e-4 3e-5 7e-5; do
       for BETA2 in 0.98 0.999; do
-        for DECAY in 1e-1 1e-2 1
+        for DECAY in 1e-1 1e-2 1e-4; do
+          for TUNING in "qof" "full"; do
+
+            for SEED in 42; do
+
+              echo "exp${counte
