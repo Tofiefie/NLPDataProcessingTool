c4dd850634dcbf6fd92dad52b6f2811dd496c37c
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-12-08 22:21:02 +0000

    remove an unused variable

--- scripts/translation.sh
+++ scripts/translation.sh
@@ -3,4 +3,4 @@ cd /home/MLTL/yiran-wang/PycharmProjects/torchglyph
 NUM_DEVICES=8 /home/MLTL/yiran-wang/miniconda3/bin/python3 -m examples train_translator \
   --study ${STUDY:-"demo"} --lr ${LR:-"7e-4"} --label-smoothing ${SMOOTH:-"0.1"} \
   --enc-self-algo ${ALGO:-"dot"} --dec-self-algo ${ALGO:-"dot"} --dec-cross-algo ${ALGO:-"dot"} \
-  --use-compile
+  --use-compile ${USECOMPILE:-"False"}
---
+++ setup.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+from setuptools import find_packages
+from se
