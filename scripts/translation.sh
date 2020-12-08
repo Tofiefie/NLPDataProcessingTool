26416abce0483244ccea0febc26a0b9e693b3e80
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-12-08 16:20:13 +0000

    dep updates

--- scripts/translation.sh
+++ scripts/translation.sh
@@ -1,3 +1,5 @@
 cd /home/MLTL/yiran-wang/PycharmProjects/torchglyph
 
-NUM_DEVICES=8 /home/MLTL/yiran-wang/miniconda3/bin/python3 -m examples tra
+NUM_DEVICES=8 /home/MLTL/yiran-wang/miniconda3/bin/python3 -m examples train_translator \
+  --study ${STUDY:-"demo"} --lr ${LR:-"7e-4"} --label-smoothing ${SMOOTH:-"0.1"} \
+  --enc-self-algo 
