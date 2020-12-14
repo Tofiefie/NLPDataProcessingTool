27c42a1830c93b76630b83885206ef7a43fa5f00
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-12-14 13:09:54 +0000

    Javadoc regenerated

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -2,3 +2,9 @@ from typing import List
 
 from torchglyph.vocab import WordPieceVocab
 from torchglyph.vocab import WordVocab
+
+
+def test_word_vocab00():
+    vocab = WordVocab[str, int](unk_token='<unk>')
+    vocab.train_from_iterator([
+        
