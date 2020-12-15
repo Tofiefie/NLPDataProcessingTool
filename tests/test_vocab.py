6f2723d162d3570dd4f328422b3aa313e5c168bb
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-12-15 11:31:20 +0000

    Update slack.md

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -7,4 +7,9 @@ from torchglyph.vocab import WordVocab
 def test_word_vocab00():
     vocab = WordVocab[str, int](unk_token='<unk>')
     vocab.train_from_iterator([
-        
+        'label1',
+        'label2',
+    ])
+
+    index1 = vocab.encode('label1')
+    index2 = v
