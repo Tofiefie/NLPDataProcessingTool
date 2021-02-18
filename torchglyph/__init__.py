4dc46d2c4ebc70dba87945d134a493c90488d630
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2021-02-18 07:29:00 +0000

    iio: imu: Use iio_trigger_get for indio_dev->trig assignment

--- torchglyph/__init__.py
+++ torchglyph/__init__.py
@@ -5,4 +5,8 @@ from pathlib import Path
 
 from matplotlib import pyplot as plt
 
-data_dir = (Path.home() / '.torchglyph').resolv
+data_dir = (Path.home() / '.torchglyph').resolve()
+if not data_dir.exists():
+    data_dir.mkdir(parents=True, exist_ok=True)
+
+host_name: str = socket.gethost
