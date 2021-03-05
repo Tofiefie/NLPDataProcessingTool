c7f08e92e9efa4b559008e875befae269bb37481
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2021-03-05 19:26:48 +0000

    1.0 release

--- torchglyph/amp.py
+++ torchglyph/amp.py
@@ -4,4 +4,9 @@ from typing import Union
 from torch import Tensor
 from torch import autocast
 from torch.cuda.amp import GradScaler
-from torch.cu
+from torch.cuda.amp import autocast
+from torch.optim import Optimizer
+
+
+class amp(object):
+    def __init__
