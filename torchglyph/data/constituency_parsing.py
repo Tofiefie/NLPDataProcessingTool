8fcfb1219acda79e28e183ee20b4af6b13a17ead
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2021-05-08 14:14:04 +0000

    protected

--- torchglyph/data/constituency_parsing.py
+++ torchglyph/data/constituency_parsing.py
@@ -1,3 +1,9 @@
 from pathlib import Path
 from typing import Any
-from typing import 
+from typing import Dict
+from typing import List
+from typing import NewType
+from typing import Tuple
+from typing import Type
+
+from datasets import
