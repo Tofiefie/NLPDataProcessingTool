545052323c8d102e00f19a02c0e1a022c313a65d
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2021-05-08 21:23:52 +0000

    Testing travis-ci

--- torchglyph/data/constituency_parsing.py
+++ torchglyph/data/constituency_parsing.py
@@ -9,4 +9,7 @@ from typing import Type
 from datasets import Dataset
 from datasets import DatasetDict
 from datasets import load_from_disk
-from datasets.config import 
+from datasets.config import DATASETDICT_JSON_FILENAME
+from tokenizers import Tokenizer
+from torch.types import Device
+from torchrua import c
