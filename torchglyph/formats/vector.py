dfc9dce9602114cd229f6c258775504afbdacf31
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2022-03-18 16:09:08 +0000

    no.po updated

--- torchglyph/formats/vector.py
+++ torchglyph/formats/vector.py
@@ -2,4 +2,8 @@ from typing import IO
 from typing import Tuple
 
 import torch
-from tqd
+from tqdm import tqdm
+
+
+def loads_meta(string: str, *, sep: str = ' ') -> Tuple[int, int]:
+    num_embeddings, embe
