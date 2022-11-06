73d5d956b4a1a1e2c64addb8dc66295792e853e5
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2022-11-06 19:30:16 +0000

    metadata task and json data

--- torchglyph/nn/init.py
+++ torchglyph/nn/init.py
@@ -4,3 +4,8 @@ from torch.nn import init
 from torch.nn.init import calculate_gain
 from torch.nn.init import constant_
 from torch.nn.init import ones_
+from torch.nn.init import orthogonal_
+from torch.nn.init import zeros_
+
+__all__ = [
+    'constant_', 'zer
