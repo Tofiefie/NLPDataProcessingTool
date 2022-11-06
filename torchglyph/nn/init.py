e4a9ff71f49718f73fd9e803b7c89beef1975644
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2022-11-06 14:36:03 +0000

    fix clippy warnings

--- torchglyph/nn/init.py
+++ torchglyph/nn/init.py
@@ -1,4 +1,6 @@
 import torch
 from torch import Tensor
 from torch.nn import init
-from torch.nn.init import c
+from torch.nn.init import calculate_gain
+from torch.nn.init import constant_
+from torch.nn.init import ones_
