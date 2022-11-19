bd39b8892c7b590ca9b708f59bf7b2328bb7d58a
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2022-11-19 07:32:53 +0000

    Update GIT.md

--- torchglyph/nn/init.py
+++ torchglyph/nn/init.py
@@ -22,4 +22,5 @@ def xavier_normal_(tensor: Tensor, fan_in: int, fan_out: int, gain: float = 1.) 
 
 
 @torch.no_grad()
-def xavier_uniform_(tensor: Tensor, fan_in: int,
+def xavier_uniform_(tensor: Tensor, fan_in: int, fan_out: int, gain: float = 1.) -> Tensor:
+    bound = gain * (6.0 / (fan_in + fan_out))
