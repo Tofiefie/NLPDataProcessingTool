c66542c9ea21ab38a7728debea06c95c3f919127
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2022-12-01 22:48:52 +0000

    Redirect

--- torchglyph/nn/plm/abc.py
+++ torchglyph/nn/plm/abc.py
@@ -5,4 +5,6 @@ from typing import Union
 from transformers import AutoConfig
 from transformers import AutoModel
 from transformers import AutoTokenizer
-from transformers import PreTrai
+from transformers import PreTrainedModel
+from transformers import PreTrainedTokenizer
+from transformers import PretrainedConfig
