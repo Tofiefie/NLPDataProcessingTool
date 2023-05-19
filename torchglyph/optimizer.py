56ebf89828b9817ce3a6f0423a2cb1443ecdeb90
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2023-05-19 20:12:09 +0000

    cleaned up project

--- torchglyph/optimizer.py
+++ torchglyph/optimizer.py
@@ -68,4 +68,5 @@ def validate_groups(module: nn.Module, with_decay: Set[nn.Parameter], without_de
 
 
 class SGD(optim.SGD):
-    def __ini
+    def __init__(self, lr: float = 1e-3, momentum: float = 0.9, dampening: float = 0.0,
+                 weig
