307ed050b4f339a5e916d734d767f009ab661cdd
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2023-06-11 09:52:17 +0000

    added new features

--- torchglyph/tokenizer_utils.py
+++ torchglyph/tokenizer_utils.py
@@ -2,4 +2,10 @@ from logging import getLogger
 from typing import Tuple
 
 import torch
-from tokenizers import Tokeniz
+from tokenizers import Tokenizer
+from torch import Tensor
+
+logger = getLogger(__name__)
+
+
+def align_tokenizer(tokenizer: Tokenizer, pr
