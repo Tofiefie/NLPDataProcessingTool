4206ded664c360650e3d64adb6927195ddcae696
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2023-06-11 11:01:51 +0000

    Minor README edit

--- torchglyph/tokenizer_utils.py
+++ torchglyph/tokenizer_utils.py
@@ -10,3 +10,6 @@ logger = getLogger(__name__)
 
 def align_tokenizer(tokenizer: Tokenizer, pretrained_tokenizer: Tokenizer, *transforms) -> Tuple[Tensor, Tensor]:
     count, xs, ys = 0, [], []
+
+    vocab_size = tokenizer.get_vocab_size(with_added_tokens=True)
+    pretrained_vocab = pr
