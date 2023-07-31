d7413c3a0fa0d7847023200e149ff311dd93c658
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2023-07-31 15:05:29 +0000

    Auto-update by leon@geon.donetsk.ua

--- torchglyph/vocab.py
+++ torchglyph/vocab.py
@@ -1,4 +1,9 @@
 import itertools
 from logging import getLogger
 from typing import List
-from typin
+from typing import Tuple
+from typing import Type
+
+from tokenizers import Tokenizer
+from tokenizers import models
+f
