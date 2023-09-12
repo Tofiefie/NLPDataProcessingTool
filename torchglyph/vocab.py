35b8b3cd8f99e9014242c1bc0929d775dfd5d4c3
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2023-09-12 09:17:02 +0000

    line length

--- torchglyph/vocab.py
+++ torchglyph/vocab.py
@@ -137,4 +137,6 @@ class WordVocab00(WordVocab):
         return [self.tokenizer.id_to_token(idx) for idx in index]
 
     def decode(self, index: Index, skip_special_tokens: bool = False) -> str:
-        return self.tokenizer.decode([index], skip_speci
+        return self.tokenizer.decode([index], skip_special_tokens=skip_special_tokens)
+
+    def decode_batch(self, indices: List[Index], skip_special_to
